fc9650059f8efba6648f8f4fec587607bfe1646d
Gopal sharma <sharmagopal7316@gmail.com>
2026-05-19 20:10:06 +0530

    initial commit

---
+++ PYTHON/oops.py
@@ -0,0 +1,10 @@
+#oops
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def display_info(self):
+        print(f"Name: {self.name}, Age: {self.age}")
+person1 = Person("Alice", 30)
+person1.display_info()
