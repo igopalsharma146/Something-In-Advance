48f9dc55d3b4371b7c97af6397af9f3d6f727b9e
Gopal sharma <sharmagopal7316@gmail.com>
2026-05-19 20:11:46 +0530

    initial commit

---
+++ PYTHON/oops 1.py
@@ -0,0 +1,11 @@
+# oops concept
+class car:
+    def __init__(self, name, model):
+        self.name = name
+        self.model = model
+
+    def display_info(self):
+        print(f"Car Name: {self.name}, Model: {self.model}")
+car1 = car("Toyota", "Camry")
+car1.display_info()
+    
