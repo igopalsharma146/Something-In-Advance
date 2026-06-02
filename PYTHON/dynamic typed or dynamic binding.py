a9815061fe9b58e1af94bc487e7c08ee3a241322
Gopal sharma <sharmagopal7316@gmail.com>
2026-06-02 21:26:45 +0530

    initial commit

---
+++ PYTHON/dynamic typed or dynamic binding.py
@@ -0,0 +1,15 @@
+# python ek dynamically typed language hai, iska matlab hai ki aapko variable declare karte waqt uska type specify karna nahi padta hai. Aap kisi bhi type ka value us variable me assign kar sakte hain, aur Python automatically uska type determine kar leta hai.
+# example
+x = 10 # x is an integer
+print(x) # output: 10
+x = "Hello, World!" # now x is a string
+print(x) # output: Hello, World!
+
+# dynamic binding ka matlab hai ki aap kisi bhi type ka value kisi bhi variable me assign kar sakte hain, aur Python automatically uska type determine kar leta hai. Iska matlab hai ki aap kisi bhi type ka value kisi bhi variable me assign kar sakte hain, aur Python automatically uska type determine kar leta hai.
+# example
+y = 20 # y is an integer
+print(y) # output: 20
+y = 3.14 # now y is a float
+print(y) # output: 3.14
+y = [1, 2, 3] # now y is a list
+print(y) # output: [1, 2, 3]
