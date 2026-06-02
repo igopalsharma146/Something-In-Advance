3c78ea15b0e710d14e75ee04742ab9538d620ea9
Gopal sharma <sharmagopal7316@gmail.com>
2026-06-02 21:22:48 +0530

    initial commit

--- PYTHON/int ki range.py
+++ PYTHON/int ki range.py
@@ -1,3 +1,6 @@
 # python me integer ki range bahut jyada hoti h
 print(10**100) # 10 ke 100 power tak integer ki range hoti h
-print(10**1000) # 10 ke 1000 power tak integer ki range hoti h
+# print(10**1000) # 10 ke 1000 power tak integer ki range hoti h
+print(1e308)
+print(1e309) # 1e309 infinity hota h
+print(-1e309) # -1e309 -infinity hota h
