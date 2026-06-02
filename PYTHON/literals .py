08f52e6dac3dbead616ab61a7790a98f91e4bf03
Gopal sharma <sharmagopal7316@gmail.com>
2026-06-02 21:31:30 +0530

    initial commit

---
+++ PYTHON/literals .py
@@ -0,0 +1,11 @@
+#literals in python
+# literals are the values that are assigned to variables. They can be of different types such as integer, float, string, boolean, etc.
+# example of literals
+x = 10 # integer literal
+y = 3.14 # float literal
+z = "Hello, World!" # string literal
+w = True # boolean literal
+print(x) # output: 10
+print(y) # output: 3.14
+print(z) # output: Hello, World!
+print(w) # output: True
